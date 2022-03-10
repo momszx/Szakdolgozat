430d57219aca0994e18d726cbc16c699fb0d1a26
Donkó Márk <55428772+momszx@users.noreply.github.com>
2022-03-10 15:54:49 +0100

    Faculty írása böngészőböl ellenőrizendő és javitando

--- Szakdolgozat/Szakdolgozat/Controllers/FacultyController.cs
+++ Szakdolgozat/Szakdolgozat/Controllers/FacultyController.cs
@@ -37,7 +37,60 @@ namespace Szakdolgozat
             }
             catch (Exception)
             {
-
+                throw;
+            }
+        }//Innentől átnézni 
+        [HttpPost]
+        public int AddFaculty(Faculty faculty){
+            try
+            {
+                DB = DatabaseManager.Instance();
+                if (DB.Connect())
+                {
+                    MySqlDataReader dataReader = DB.DataReader(string.Format("INSERT INTO faculty(name)value('{0}')",faculty.name));
+                    dataReader.Close();
+                    DB.Close();
+                }
+                return 1;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+         [HttpPatch]
+        public int AddFaculty(Faculty faculty){
+            try
+            {
+                DB = DatabaseManager.Instance();
+                if (DB.Connect())
+                {
+                    MySqlDataReader dataReader = DB.DataReader(string.Format("UPDATE faculty set name='{0}' where id='{1}'",faculty.name,faculty.id));
+                    dataReader.Close();
+                    DB.Close();
+                }
+                return 1;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+         [HttpDelete]
+        public int AddFaculty(Faculty faculty){
+            try
+            {
+                DB = DatabaseManager.Instance();
+                if (DB.Connect())
+                {//először tötölni a jegyzet és a kérdés kommenteket majd törölni a jegyzeteket és a kérdéseket majd a tárgyat majd képzést majd a szakot
+                    MySqlDataReader dataReader = DB.DataReader(string.Format("delete faculty where id='{0}'",faculty.id));
+                    dataReader.Close();
+                    DB.Close();
+                }
+                return 1;
+            }
+            catch (Exception)
+            {
                 throw;
             }
         }
