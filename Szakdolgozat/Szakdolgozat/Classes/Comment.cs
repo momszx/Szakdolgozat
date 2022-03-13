335fee452c5467d628a522b9de969d3a59fb2d3f
momsz <donkomark@gmail.com>
2022-03-13 14:57:55 +0100

    Commentek és topikok

---
+++ Szakdolgozat/Szakdolgozat/Classes/Comment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Szakdolgozat.Classes
+{
+    public class Comment : BaseClass
+    {
+        private int topicId;
+        private int userId;
+        private string text;
+        private DateTime dateTime;
+        private string themeType;
+
+        public Comment(int id, int topicId, int userId, string text, string Uid, DateTime dateTime,string themeType) : base(id, Uid)
+        {
+            TopicId = topicId;
+            UserId = userId;
+            Text = text;
+            DateTime = dateTime;
+            ThemeType = themeType;  
+        }
+
+        public int TopicId { get => topicId; set => topicId = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public string Text { get => text; set => text = value; }
+        public DateTime DateTime { get => dateTime; set => dateTime = value; }
+        public string ThemeType { get => themeType; set => themeType = value; }
+    }
+}
--- Szakdolgozat/Szakdolgozat/Classes/Topic.cs
+++ Szakdolgozat/Szakdolgozat/Classes/Topic.cs
@@ -2,25 +2,25 @@
 
 namespace Szakdolgozat.Classes
 {
-    public class Note : BaseClass
+    public class Topic : BaseClass
     {
         private int userId;
         private int subjectId;
         private string name;
-        private string noteWrite;
+        private string text;
         private DateTime dateTime;
-        public Note(int id, int userId, int subjectId, string name, string noteWrite, string Uid, DateTime dateTime) : base(id, Uid)
+        public Topic(int id, int userId, int subjectId, string name, string text, string Uid, DateTime dateTime) : base(id, Uid)
         {
             UserId = userId;
             SubjectId = subjectId;
             Name = name;
-            NoteWrite = noteWrite;
+            Text = text;
             DateTime= dateTime;
         }
 
         public int SubjectId { get => subjectId; set => subjectId = value; }
         public string Name { get => name; set => name = value; }
-        public string NoteWrite { get => noteWrite; set => noteWrite = value; }
+        public string Text { get => text; set => text = value; }
         public DateTime DateTime { get => dateTime; set => dateTime = value; }
         public int UserId { get => userId; set => userId = value; }
     }
